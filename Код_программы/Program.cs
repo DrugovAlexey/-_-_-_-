2a1285bc57112f86cc32fccf3fe8a352bfb323f9
Olga <panda146@yandex.ru>
2023-08-03 15:52:44 +0300

    added output array code

--- Код_программы/Program.cs
+++ Код_программы/Program.cs
@@ -4,5 +4,18 @@
     for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
     Console.WriteLine();
 }
-
+void FindArray(string[] OutputArray)
+{
+    Console.Write($"Итоговый массив: ");
+    string[] newarray = new string[OutputArray.Length];
+    for (int i = 0; i < OutputArray.Length; i++)
+    {
+        if (OutputArray[i].Length <= 3)
+        {
+            newarray[i] = OutputArray[i];
+            Console.Write($"{newarray[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
 
