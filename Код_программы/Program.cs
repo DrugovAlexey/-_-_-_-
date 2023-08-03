ba85324e89bc78d2441406412304a63ec8098b5a
Olga <panda146@yandex.ru>
2023-08-03 15:48:44 +0300

    added file code

---
+++ Код_программы/Program.cs
@@ -0,0 +1,8 @@
+﻿void EnterArray(string[] InputArray)
+{
+    Console.Write($"Изначальный массив: ");
+    for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
+    Console.WriteLine();
+}
+
+
